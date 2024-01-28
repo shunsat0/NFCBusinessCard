97cbd0a7c2c97cea99c0b0079f5fa6c8cc24276e
shun <ellegardenno8@gmail.com>
2024-01-28 22:03:33 +0900

    Initial Commit

---
+++ NFCBusinessCard/NFCBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCBusinessCardApp.swift
+//  NFCBusinessCard
+//
+//  Created by Shun Sato on 2024/01/28.
+//
+
+import SwiftUI
+
+@main
+struct NFCBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
