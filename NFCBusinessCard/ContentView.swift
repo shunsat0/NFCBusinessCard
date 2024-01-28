3dc178cca7f7f9bd08686944f2cfb8eb2c9a572f
shun <ellegardenno8@gmail.com>
2024-01-28 23:07:51 +0900

    add : テキストフィールドとボタンを追加

--- NFCBusinessCard/ContentView.swift
+++ NFCBusinessCard/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputUrl = "https://example.com/"
+    @FocusState var isFocused: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Spacer()
+                TextField("URLを入力してください", text: $inputUrl)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .focused($isFocused)
+                    .keyboardType(.URL)
+                    .onSubmit {
+                        // returnされた時の処理
+                        print("\(inputUrl)")
+                    }
+                
+                Spacer()
+                
+                Button(action: {
+                    isFocused = false
+                }, label: {
+                    Text("NFCカードに登録")
+                        .font(.system(size: 20,weight: .bold,design: .default))
+
+                        .padding()
+                })
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(10.0)
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
