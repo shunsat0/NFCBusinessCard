ec42dcd23e049f20ebbca907eea5c6271295c8ed
youten410 <techregisteremail@gmail.com>
2024-03-27 18:10:48 +0900

    update : navigationと一覧を追加

--- NFCBusinessCard/ContentView.swift
+++ NFCBusinessCard/ContentView.swift
@@ -14,15 +14,15 @@ struct ContentView: View {
     @FocusState var isFocused: Bool
     @ObservedObject var NFCR = NFCReader()
     @ObservedObject var NFCW = NFCWriter()
-        
+    
     
     func read() {
         NFCR.read()
         readedUrl = NFCR.raw
         print(readedUrl)
     }
-
-
+    
+    
     
     func write() {
         NFCW.msg = inputUrl
@@ -30,50 +30,61 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            
-            Text("\(readedUrl)")
-            
-            Spacer()
-            TextField("URLを入力してください", text: $inputUrl, axis: .vertical)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .focused($isFocused)
-                .keyboardType(.URL)
-            
-            //Text("\(NFCR.raw)")
-            
-            Spacer()
-            
-            HStack {
-                Button(action: {
-                    write()
-                    NFCW.type = "U"
-                    isFocused = false
-                }, label: {
-                    Text("書き込み")
-                        .font(.system(size: 20,weight: .bold,design: .default))
-                        .padding()
-                })
-                .foregroundColor(.white)
-                .background(.blue)
-                .cornerRadius(10.0)
-                .padding()
+        NavigationView {
+            VStack {
                 
-                Button(action: {
-                    read()
-                }, label: {
-                    Text("読み込み")
-                        .font(.system(size: 20,weight: .bold,design: .default))
-                        .padding()
-                })
-                .foregroundColor(.accentColor)
-                .background(.gray)
-                .cornerRadius(10.0)
-                .padding()
+                Text("\(readedUrl)")
+                
+                Spacer()
+                TextField("URLを入力してください", text: $inputUrl, axis: .vertical)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .focused($isFocused)
+                    .keyboardType(.URL)
+                
+                Spacer()
+                
+                HStack {
+                    Button(action: {
+                        write()
+                        NFCW.type = "U"
+                        isFocused = false
+                    }, label: {
+                        Text("書き込み")
+                            .font(.system(size: 20,weight: .bold,design: .default))
+                            .padding()
+                    })
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10.0)
+                    .padding()
+                    
+                    Button(action: {
+                        read()
+                    }, label: {
+                        Text("読み込み")
+                            .font(.system(size: 20,weight: .bold,design: .default))
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.accentColor, lineWidth: 3)
+                            )
+                    })
+                    .foregroundColor(.accentColor)
+                    .background(.white)
+                    .padding()
+                }
+                Spacer()
             }
-            Spacer()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: ListView()) {
+                        Text("交換した人一覧")
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
